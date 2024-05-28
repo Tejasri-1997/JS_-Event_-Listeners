30e97486b6b0530faa49fdaa4f6bec30e883bab6
Kothuri Tejasri <145921897+Tejasri-1997@users.noreply.github.com>
2024-05-28 10:36:47 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,78 @@
+// Events & event Listeners
+const darkTheme = function(){
+    document.querySelector("body").style.backgroundColor="black"
+    document.querySelector(".dark-theme").style.backgroundColor="Light Theme"
+    document.querySelector("header").style.backgroundColor="#2E282A"
+    document.querySelector(".sidenav").style.backgroundColor="#808A9F"
+}
+const lightTheme=function(){
+    document.querySelector("body").style.backgroundColor="white"
+    document.querySelector(".dark-theme").style.backgroundColor="Dark Theme"
+    document.querySelector("header").style.backgroundColor="#F86624"
+    document.querySelector(".sidenav").style.backgroundColor="#1768AC"
+}
+function eleHover(element){
+    element.style.backgroundColor="yellow";
+}
+function eleOut(item){
+    item.style.backgroundColor="#DEE7E7";
+}
+const changeCompany=function(){
+    document.querySelector("#company").textContent="sales force";
+};
+const normal = function() {
+    document.querySelector("#company").textContent = "Company"
+}
+
+const keyPressed = function() {
+    document.querySelector("body").style.backgroundColor = "white"
+}
+
+const keyDown = function() {
+    document.querySelector("body").style.backgroundColor = "red"
+}
+
+// AddEventListener
+
+const loginButton = document.getElementById("loginBtn")
+const loginModal= document.getElementById("loginSection")
+const sideNavBar = document.querySelector(".sidenav")
+const mainSection = document.querySelectorAll(".main .section")
+
+loginButton.onclick = showLogin
+
+function showLogin(){
+    sideNavBar.style.display="none";
+    mainSection.forEach(function(item){
+        item.style.display = "none"
+    })
+    loginModal.style.display="block"
+}
+document.getElementById("closeBtn").addEventListener('click',hideLogin)
+
+function hideLogin(){
+    sideNavBar.style.display="block";
+    mainSection.forEach(function(item){
+        item.style.display = "block"
+    })
+    loginModal.style.display="none"
+}
+//Add & Remove Event Listeners
+
+document.getElementById("loginSubmit").addEventListener('mouseover',function(){
+    this.style.background='green'
+})
+
+document.getElementById("loginSubmit").addEventListener('mouseout',function(){
+    this.style.background='blue'
+})
+
+const NavBorder= document.getElementById("add-border")
+NavBorder.addEventListener("click",addingBorder)
+function addingBorder(){
+    document.querySelector("header").style.border="5px solid black"
+
+}
+if(10>20){
+    NavBorder.removeEventListener("click",addingBorder)
+}
